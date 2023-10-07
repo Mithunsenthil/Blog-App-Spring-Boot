be83a48b260a4b44cf5085772d66cca0ed496064
Mithunsenthil <mithunsenthil012@gmail.com>
2023-10-07 15:09:14 +0530

    commit

--- blog/src/main/java/com/example/blog/controllers/PostController.java
+++ blog/src/main/java/com/example/blog/controllers/PostController.java
@@ -3,6 +3,7 @@ package com.example.blog.controllers;
 import com.example.blog.entities.Post;
 import com.example.blog.services.PostServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
